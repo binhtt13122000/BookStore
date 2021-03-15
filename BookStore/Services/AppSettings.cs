5ead0431d3f028c1d5463fe90d0edbda09c4cab0
Tiktokcity <danhskipper18@gmail.com>
2021-03-15 12:36:57 +0700

    [DanhPV] create api

---
+++ BookStore/Services/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Services
+{
+    public class AppSettings
+    {
+        public String Secret { get; set; }
+    }
+}
